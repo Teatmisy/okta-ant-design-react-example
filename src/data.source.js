6bead3d8454b94f353f3be02ee0eefa74c603f56
fishbowlsoftware <nik@fishbowlllc.com>
2020-09-16 13:37:33 -0400

    initial commit

---
+++ src/data.source.js
@@ -0,0 +1,114 @@
+import React from 'react';
+export const Banner50DataSource = {
+  wrapper: { className: 'home-page-wrapper banner5' },
+  page: { className: 'home-page banner5-page' },
+  childWrapper: {
+    className: 'banner5-title-wrapper',
+    children: [
+      { name: 'title', children: '产品名', className: 'banner5-title' },
+      {
+        name: 'explain',
+        className: 'banner5-explain',
+        children: '产品标语介绍',
+      },
+      {
+        name: 'content',
+        className: 'banner5-content',
+        children: '产品的详细说明，如是什么东西之类的文字',
+      },
+      {
+        name: 'button',
+        className: 'banner5-button-wrapper',
+        children: {
+          href: '#',
+          className: 'banner5-button',
+          type: 'primary',
+          children: '开始使用',
+        },
+      },
+    ],
+  },
+  image: {
+    className: 'banner5-image',
+    children:
+      'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*-wAhRYnWQscAAAAAAAAAAABkARQnAQ',
+  },
+};
+export const Content50DataSource = {
+  wrapper: { className: 'home-page-wrapper content5-wrapper' },
+  page: { className: 'home-page content5' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      { name: 'title', children: '客户案例', className: 'title-h1' },
+      {
+        name: 'content',
+        className: 'title-content',
+        children: '在这里用一段话介绍服务的案例情况',
+      },
+    ],
+  },
+  block: {
+    className: 'content5-img-wrapper',
+    gutter: 16,
+    children: [
+      {
+        name: 'block0',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://t.alipayobjects.com/images/rmsweb/T11aVgXc4eXXXXXXXX.svg',
+          },
+          content: { children: 'Ant Design' },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://zos.alipayobjects.com/rmsportal/faKjZtrmIbwJvVR.svg',
+          },
+          content: { children: 'Ant Motion' },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://t.alipayobjects.com/images/rmsweb/T11aVgXc4eXXXXXXXX.svg',
+          },
+          content: { children: 'Ant Design' },
+        },
+      },
+      {
+        name: 'block3',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://zos.alipayobjects.com/rmsportal/faKjZtrmIbwJvVR.svg',
+          },
+          content: { children: 'Ant Motion' },
+        },
+      },
+    ],
+  },
+};
